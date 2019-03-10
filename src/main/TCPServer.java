0a12c9a57a1b6288f1f22a674aa67d27b5f23e27
wq <735462752@qq.com>
2019-03-10 13:32:33 +0800

    socket编程简例

---
+++ src/main/TCPServer.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(65256);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            new LengthCalculator(socket).start();
+        }
+    }
+}
